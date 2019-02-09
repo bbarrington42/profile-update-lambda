e03f399f47a130df46afc120609267aa6d14bd62
Bill Barrington <bill.barrington@candidpartners.com>
2019-02-09 09:16:24 -0500

    Prep for deployment.

--- handler.js
+++ handler.js
@@ -2,25 +2,11 @@
 
 const db = require('./lib/db');
 
-// module.exports.hello = async (event, context) => {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({
-//       message: 'Go Serverless v1.0! Your function executed successfully!',
-//       input: event,
-//     }),
-//   };
-//
-//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-// };
-
-
 // The trigger for this handler is an upload of the CSV file to S3.
 // todo Add content validation
 
 
-// For now, just use an array of test data
+// An array of test data
 const input = [
     "country, language, brand_name, active, caffeine, exclusive, kitName, lowCal, material, sparkling, image_url, rank",
     "'US', 'en', 'Coca-Cola', 1, 1, 1, 'Coke', 1, 1234567, 1, 'http://blah-blah', 1",
@@ -29,10 +15,15 @@ const input = [
 ];
 
 
-const handler = (input) => db.run(input, function (err, result) {
-    if (err) console.error(`error: ${JSON.stringify(err)}`);
-    else console.log(`success: ${JSON.stringify(result)}`);
-});
+exports.addBeverage = async (event, context) => {
+    // Examine event and create input for DB update
+    console.log(`Event: ${JSON.stringify(event)}`);
+    console.log(`Context: ${JSON.stringify(context)}`);
 
+    // todo for now use dummy input just to see if we can connect
+    db.run(input, function (err, result) {
+        if (err) console.error(`error: ${JSON.stringify(err)}`);
+        else console.log(`success: ${JSON.stringify(result)}`);
+    })
+};
 
-handler(input);

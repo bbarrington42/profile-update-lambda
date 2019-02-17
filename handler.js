fedc9467f4f19d3983e2692b1eec9940a906e476
Bill Barrington <bill.barrington@candidpartners.com>
2019-02-17 17:27:46 -0500

    Added sql gen for updates.

--- handler.js
+++ handler.js
@@ -8,47 +8,6 @@ const S3 = require('aws-sdk/clients/s3');
 const s3 = new S3();
 
 
-// Example event structure:
-const event = {
-    "Records": [
-        {
-            "eventVersion": "2.1",
-            "eventSource": "aws:s3",
-            "awsRegion": "us-east-1",
-            "eventTime": "2019-02-09T19:59:21.346Z",
-            "eventName": "ObjectCreated:Put",
-            "userIdentity": {
-                "principalId": "AWS:AIDAJ4WE6WYXUXG66XSPM"
-            },
-            "requestParameters": {
-                "sourceIPAddress": "136.55.20.250"
-            },
-            "responseElements": {
-                "x-amz-request-id": "0B90EFD112B8CB49",
-                "x-amz-id-2": "Vm2UcI8i8cZ+G64+sE+9FhKJVcT3al+A2wziZAAxBvddpHqxzjKVTb+rAk1AvYRt1zUkN1zMS/4="
-            },
-            "s3": {
-                "s3SchemaVersion": "1.0",
-                "configurationId": "dd14a857-feab-4682-b1bd-0f5f923030cb",
-                "bucket": {
-                    "name": "ccfs-misc-dev",
-                    "ownerIdentity": {
-                        "principalId": "A2XG4F6ZSJ1FVZ"
-                    },
-                    "arn": "arn:aws:s3:::ccfs-misc-dev"
-                },
-                "object": {
-                    "key": "beverage-profile-update/profile-update1.csv",
-                    "size": 190,
-                    "eTag": "3744d236c544738bd614f7db8419ad01",
-                    "sequencer": "005C5F311948BCE428"
-                }
-            }
-        }
-    ]
-};
-
-
 const run = (bucket, key, config) => {
     console.log(`Invoking s3.getObject: bucket = ${bucket}, key = ${key}`);
     return util.getObjectAsString(s3, bucket, key).then(data => {
